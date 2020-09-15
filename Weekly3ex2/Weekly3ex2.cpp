2f0c27efc0eeb4da3abc63abd9379eaa22b95f37
YleniaChitano <116096@stud.inn.no>
2020-09-15 09:05:09 +0200

    Removed useless stuff

--- Weekly3ex2/Weekly3ex2.cpp
+++ Weekly3ex2/Weekly3ex2.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
-#include <cstdlib>
+#include <cstdlib> // Needed for srand ()
+#include <ctime> // Needed for time (0)
 
 
 int main() {
-    std::srand(8346);
-beginning:
-    int times{ 1 };
-    int random = std::rand();
-    int guess;
-question:
+    srand(std::time(0)); // Generates a new random number, using time as a seed.
+beginning: // Will begin from here when a new random number is needed.
+    int times{ 1 }; // Guess counter variable that increments every time the user guesses a number.
+    int random = rand(); // "random" is a fixed random number taken from srand above.
+    int guess; // The guessed number
+
     std::cout << "Try to guess the number I have thought. Please enter a number:\n";
+question: // Will go back here when the guessed number is wrong.
     std::cin >> guess;
-    
-
-
-
 
     if (guess == random) {
         char yn;
         std::cout << "That's correct! You won! It took you " << times << " guess(es). Do you want to play again (Y/N) ? \n";
         std::cin >> yn;
-        yn = toupper(yn);
+        yn = toupper(yn); // Makes yn case insensitive.
         if (yn == 'Y') {
-            int random = std::rand();
             goto beginning;
         }
 
@@ -31,6 +28,7 @@ question:
 
     else {
         times++;
+
         if (guess < random) {
             std::cout << "Nope, try a higher number!\n";
             goto question;
